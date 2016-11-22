2ce865948b629fb402d155cc1dda1ebacfdae0d9
YoungseobEum <eys257@gmail.com>
2016-11-22 15:38:28 -0500

    change

--- GWRR_sample.R
+++ GWRR_sample.R
@@ -2,14 +2,12 @@ library(gwrr)
 data(columbus)
 locs <- cbind(columbus$x, columbus$y)
 
-
 # GWR
 #col.gwr <- gwr.est(crime ~ income + houseval, locs, columbus, "exp")
 col.bw <- gwr.bw.est(crime ~ income + houseval, locs, columbus, "exp")
 col.gwr <- gwr.est(crime ~ income + houseval, locs, columbus, "exp", bw=col.bw$phi)
 plot(col.gwr$beta[2,], col.gwr$beta[3,])
 plot(columbus$x, columbus$y, cex=col.gwr$beta[1,]/10)
-
 
 # diagnostic
 col.bw <- gwr.bw.est(crime ~ income + houseval, locs, columbus, "exp")
@@ -21,7 +19,6 @@ col.gwrr <- gwrr.est(crime ~ income + houseval, locs, columbus, "exp", bw=2.00, 
 plot(col.gwrr$beta[2,], col.gwrr$beta[3,])
 plot(columbus$x, columbus$y, cex=col.gwrr$beta[1,]/10)
 col.gwr <- gwrr.est(crime ~ income + houseval, locs, columbus, "exp", bw=col.gwrr$phi, rd=0)
-
 
 # GW lasso
 col.gwl <- gwl.est(crime ~ income + houseval, locs, columbus, "exp")
