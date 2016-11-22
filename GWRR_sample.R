e26dd8e8f66863f01707bbf74d35010cbf114299
YoungseobEum <eys257@gmail.com>
2016-11-22 12:23:08 -0500

    Sample code

---
+++ GWRR_sample.R
@@ -0,0 +1,22 @@
+library(gwrr)
+data(columbus)
+locs <- cbind(columbus$x, columbus$y)
+
+
+# GWR
+col.bw <- gwr.bw.est(crime ~ income + houseval, locs, columbus, "exp")
+col.gwr <- gwr.est(crime ~ income + houseval, locs, columbus, "exp", bw=col.bw$phi)
+plot(col.gwr$beta[2,], col.gwr$beta[3,])
+plot(columbus$x, columbus$y, cex=col.gwr$beta[1,]/10)
+
+
+# diagnostic
+col.bw <- gwr.bw.est(crime ~ income + houseval, locs, columbus, "exp")
+col.vdp <- gwr.vdp(crime ~ income + houseval, locs, columbus, col.bw$phi, "exp")
+hist(col.vdp$condition)
+
+# GW ridge regression
+col.gwrr <- gwrr.est(crime ~ income + houseval, locs, columbus, "exp", bw=2.00, rd=0.03) 
+plot(col.gwrr$beta[2,], col.gwrr$beta[3,])
+plot(columbus$x, columbus$y, cex=col.gwrr$beta[1,]/10)
+col.gwr <- gwrr.est(crime ~ income + houseval, locs, columbus, "exp", bw=col.gwrr$phi, rd=0)
